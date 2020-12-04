590c4e8c5e97fd27e9699b55f6a356a041af633a
Paul Sweeney <paul@8-bitdesign.com>
2020-12-04 12:54:55 -0800

    Day 3!

---
+++ 03/1.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const map = fs.readFileSync('./input', 'utf8').split('\n').filter(l => l).map(l => l.split(''))
+
+function check (xInc = 1, yInc = 1, map) {
+  let x
+
+  return map.filter((l, index) => index % yInc - 1).reduce((count, line) => {
+    if (typeof x === 'undefined') {
+      x = 0
+    } else {
+      x = (x + xInc) % line.length
+    }
+    return (line[x] === '#') ? count + 1 : count
+  }, 0)
+}
+
+console.log(check(3, 1, map))
---
+++ 03/2.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const map = fs.readFileSync('./input', 'utf8').split('\n').filter(l => l).map(l => l.split(''))
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+
+function check (xInc = 1, yInc = 1, map) {
+  let x
+
+  return map.filter((l, index) => index % yInc - 1).reduce((count, line) => {
+    if (typeof x === 'undefined') {
+      x = 0
+    } else {
+      x = (x + xInc) % line.length
+    }
+    return (line[x] === '#') ? count + 1 : count
+  }, 0)
+}
+
+console.log(slopes.map(([x, y]) => check(x, y, map)).reduce((a, b) => a * b))
